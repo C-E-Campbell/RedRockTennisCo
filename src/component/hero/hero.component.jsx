c586a8539982b9e7e455f97aa1a16c477f5ce978
charles <codelyfewithcharlie@gmail.com>
2019-09-14 11:13:23 -0700

    added to hero and font stack

--- src/component/hero/hero.component.jsx
+++ src/component/hero/hero.component.jsx
@@ -4,13 +4,22 @@ import "./hero.style.scss";
 const Hero = () => {
 	return (
 		<div className='hero-container'>
-			<div className='hero-box'>
-				<h1>Hello</h1>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor,
-					fugiat.
-				</p>
-				<a>CTA BTN</a>
+			<div className='hero-box-container'>
+				<div className='hero-box'>
+					<h1>Get Gear.</h1>
+					<h1>Check Stats.</h1>
+					<h1>Train.</h1>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor,
+						fugiat.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+						dolores.
+					</p>
+					<a className='hero-btn hero-btn-primary'>Visit Store</a>
+					<a className='hero-btn hero-btn-secondary'>Check the Rankings</a>
+				</div>
 			</div>
 		</div>
 	);
