f7907dd343dec016e7a56435fd429139fbdef339
charles <codelyfewithcharlie@gmail.com>
2019-09-14 10:16:13 -0700

    added hero cta section

--- src/component/hero/hero.component.jsx
+++ src/component/hero/hero.component.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./hero.style.scss";
 
 const Hero = () => {
-	return <div className='hero-container'></div>;
+	return (
+		<div className='hero-container'>
+			<div className='hero-box'>
+				<h1>Hello</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor,
+					fugiat.
+				</p>
+				<a>CTA BTN</a>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
