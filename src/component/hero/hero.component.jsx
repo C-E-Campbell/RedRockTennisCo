40ac934084fd6ceaaf3e2680a439d135ed3cb9f4
charles <codelyfewithcharlie@gmail.com>
2019-09-14 12:03:56 -0700

    style changes

--- src/component/hero/hero.component.jsx
+++ src/component/hero/hero.component.jsx
@@ -9,13 +9,10 @@ const Hero = () => {
 					<h1>Get Gear.</h1>
 					<h1>Check Stats.</h1>
 					<h1>Train.</h1>
+					<p>Welcome to the #1 Online Place for all things tennis</p>
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor,
-						fugiat.
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-						dolores.
+						Check upcoming matches or snag some new shoes.{" "}
+						<span>One Stop Shop!</span>
 					</p>
 					<a className='hero-btn hero-btn-primary'>Visit Store</a>
 					<a className='hero-btn hero-btn-secondary'>Check the Rankings</a>
