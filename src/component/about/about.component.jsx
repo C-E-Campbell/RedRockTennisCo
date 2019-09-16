5be39424cee7008e29e353575da41658161241f1
charles <codelyfewithcharlie@gmail.com>
2019-09-15 17:17:09 -0700

    added blog area

--- src/component/about/about.component.jsx
+++ src/component/about/about.component.jsx
@@ -4,29 +4,20 @@ import "./about.style.scss";
 const About = () => {
 	return (
 		<div className='about-section'>
-			<div className='about-item'>
-				<h2>ICON</h2>
-				<h2>LINE 1</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-					laborum doloribus provident consectetur corrupti accusantium!
-				</p>
+			<div className='about-item item-75'>
+				<h2>Get Ready for the Upcoming Tourney</h2>
+
+				<p>Who will take the womens chamionship at Wemberly?</p>
+				<p>Barty? Pliskova? Svitolina?</p>
+				<a href='#'>Read more...</a>
 			</div>
-			<div className='about-item'>
-				<h2>ICON</h2>
-				<h2>LINE 1</h2>
+			<div className='about-item item-25'>
+				<h2>Best Racquets For Beginners</h2>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
 					laborum doloribus provident consectetur corrupti accusantium!
 				</p>
-			</div>
-			<div className='about-item'>
-				<h2>ICON</h2>
-				<h2>LINE 1</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-					laborum doloribus provident consectetur corrupti accusantium!
-				</p>
+				<a href='#'>Read more...</a>
 			</div>
 		</div>
 	);
